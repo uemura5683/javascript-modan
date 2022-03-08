b91b68815f6c9c131294ff40c01d5854ea778ac9
uemuragame5683 <nobuyoshi5683@gmail.com>
2022-03-09 01:13:07 +0900

    filter学習

--- filter/index.js
+++ filter/index.js
@@ -6,7 +6,9 @@ function isBigEnough(value) {
   return value >= 10
 }
 let filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
-console.log(filtered);
+
+console.log( 'filter' );
+console.log( filtered );
 
 /**
 * find
@@ -22,9 +24,8 @@ function isCherries(fruit) {
   return fruit.name === 'cherries';
 }
 
-console.log(inventory.find(isCherries));
-// { name: 'cherries', quantity: 5 }
-
+console.log( 'find' );
+console.log( inventory.find( isCherries ) );
 
 /**
 * findIndex
@@ -33,8 +34,9 @@ console.log(inventory.find(isCherries));
 let fruits = ["apple", "banana", "cantaloupe", "blueberries", "grapefruit"];
 let index = fruits.findIndex(fruit => fruit === "blueberries");
 
-console.log(index); // 3
-console.log(fruits[index]); // blueberries
+console.log( 'findIndex' );
+console.log( index );
+console.log( fruits[index] );
 
 /**
 * some
@@ -44,8 +46,9 @@ function checkAvailability(arr, val) {
   return arr.some(arrVal => val === arrVal);
 }
 
-checkAvailability(fruits, 'kela');   // false
-checkAvailability(fruits, 'banana'); // true
+console.log( 'some' );
+checkAvailability( fruits, 'kela' );
+checkAvailability( fruits, 'banana' );
 
 /**
 * every
@@ -54,16 +57,18 @@ checkAvailability(fruits, 'banana'); // true
 function isBigEnough(element, index, array) {
   return element >= 10;
 }
-[12, 5, 8, 130, 44].every(isBigEnough);   // false
-[12, 54, 18, 130, 44].every(isBigEnough); // true
+[12, 5, 8, 130, 44].every(isBigEnough);
+[12, 54, 18, 130, 44].every(isBigEnough);
 
 /**
 * reverse
 **/
+console.log( 'reverce' );
 const reverses = {0: 1, 1: 2, 2: 3, length: 3};
-console.log(reverses); // {0: 1, 1: 2, 2: 3, length: 3}
-Array.prototype.reverse.call(reverses); //apply() を使用するのと同じ構文
-console.log(reverses); // {0: 3, 1: 2, 2: 1, length: 3}
+console.log(reverses);
+
+Array.prototype.reverse.call(reverses);
+console.log(reverses);
 
 /**
 * shift
@@ -71,35 +76,13 @@ console.log(reverses); // {0: 3, 1: 2, 2: 1, length: 3}
 **/
 var myFish = ['angel', 'clown', 'mandarin', 'surgeon'];
 
+console.log( 'shift' );
 console.log('myFish before:', JSON.stringify(myFish));
-// myFish 処理前: ['angel', 'clown', 'mandarin', 'surgeon']
 
 var shifted = myFish.shift();
 
 console.log('myFish 処理後:', myFish);
-// myFish 処理後: ['clown', 'mandarin', 'surgeon']
-
 console.log('取り除いた要素:', shifted);
-// 取り除いた要素: angel
-
-/**
-* unshift
-* https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-**/
-let unshift = [1, 2]
-
-unshift.unshift(0)               // 呼び出しの返値は 3、新しい配列の長さ
-// arr is [0, 1, 2]
-
-unshift.unshift(-2, -1)          // 新しい配列の長さは 5
-// arr is [-2, -1, 0, 1, 2]
-
-unshift.unshift([-4, -3])        // 新しい配列の長さは 6
-// arr is [[-4, -3], -2, -1, 0, 1, 2]
-
-unshift.unshift([-7, -6], [-5])  // 新しい配列の長さは 8
-// arr is [ [-7, -6], [-5], [-4, -3], -2, -1, 0, 1, 2 ]
-
 
 /**
 * pop
@@ -107,9 +90,10 @@ unshift.unshift([-7, -6], [-5])  // 新しい配列の長さは 8
 **/
 let popped = myFish.pop();
 
-console.log(myFish); // ['angel', 'clown', 'mandarin' ]
+console.log( 'pop' );
+console.log(myFish);
 
-console.log(popped); // 'sturgeon'
+console.log(popped);
 
 /**
 * push
@@ -118,33 +102,71 @@ console.log(popped); // 'sturgeon'
 let vegetables = ['parsnip', 'potato']
 let moreVegs = ['celery', 'beetroot']
 
-// 1 つ目の配列に 2 つ目の配列をマージさせます
-// vegetables.push('celery', 'beetroot'); と同じ結果になります
 Array.prototype.push.apply(vegetables, moreVegs)
 
-console.log(vegetables)  // ['parsnip', 'potato', 'celery', 'beetroot']
+console.log( 'push' );
+console.log(vegetables)
 
 /**
-* map
-* https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+* at
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/at
 **/
-// 比較のために、上記の配列に parseInt() を用いると次のようになります。
-['1.1', '2.2e2', '3e300'].map( str => parseInt(str) ) // [1, 2, 3]
+
+const array1 = [5, 12, 8, 130, 44];
+let indexat = 4;
+
+console.log( 'at' );
+console.log(`Using an index of ${indexat} the item returned is ${array1.at(indexat)}`);
+
+indexat = -2;
+console.log(`Using an index of ${indexat} item returned is ${array1.at(indexat)}`);
+
 
 /**
 * fill
-* https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
 **/
-[1, 2, 3].fill(4)                // [4, 4, 4]
-[1, 2, 3].fill(4, 1)             // [1, 4, 4]
-[1, 2, 3].fill(4, 1, 2)          // [1, 4, 3]
-[1, 2, 3].fill(4, 1, 1)          // [1, 2, 3]
-[1, 2, 3].fill(4, 3, 3)          // [1, 2, 3]
-[1, 2, 3].fill(4, -3, -2)        // [4, 2, 3]
-[1, 2, 3].fill(4, NaN, NaN)      // [1, 2, 3]
-[1, 2, 3].fill(4, 3, 5)          // [1, 2, 3]
-Array(3).fill(4)                 // [4, 4, 4]
-[].fill.call({ length: 3 }, 4)   // {0: 4, 1: 4, 2: 4, length: 3}
 
-let fillarr = Array(3).fill({}) // [{}, {}, {}]
-fillarr[0].hi = "hi"            // [{ hi: "hi" }, { hi: "hi" }, { hi: "hi" }]
+const array_fill = [1, 2, 3, 4];
+
+console.log( 'fill' );
+console.log(array_fill.fill(0, 2, 4));
+console.log(array_fill.fill(5, 1));
+console.log(array_fill.fill(6));
+
+/**
+* join
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+**/
+
+const elements_join = ['Fire', 'Air', 'Water'];
+
+console.log( 'join' );
+console.log(elements_join.join());
+console.log(elements_join.join(''));
+console.log(elements_join.join('-'));
+
+/**
+* unshift
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+**/
+const array_unshift = [1, 2, 3];
+
+console.log( 'unshift' );
+console.log(array_unshift.unshift(4, 5));
+console.log(array1);
+
+/**
+* includes
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+**/
+console.log( 'includes' );
+
+const array_includes = [1, 2, 3];
+
+console.log(array_includes.includes(2));
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+console.log(pets.includes('at'));
